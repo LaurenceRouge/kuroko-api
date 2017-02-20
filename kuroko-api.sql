b4237f6b90c03b5446ad5933ebc64df7ab12755c
LaurenceRouge <laurence.rouge@hetic.net>
2017-02-20 09:46:11 +0100

    Database sql export

---
+++ kuroko-api.sql
@@ -0,0 +1,123 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.1
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:8889
+-- Généré le :  Lun 20 Février 2017 à 09:45
+-- Version du serveur :  5.6.34
+-- Version de PHP :  7.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `kuroko-api`
+--
+
+--
+-- Contenu de la table `personage`
+--
+
+INSERT INTO `personage` (`id`, `name`, `gender`) VALUES
+(1, 'Seijūrō Akashi', 'Male'),
+(2, 'Shintarō Midorima', 'Male'),
+(3, 'Ryōta Kise', 'Male'),
+(5, 'Daiki Aomine', 'Male'),
+(6, 'Atsushi Murasakibara', 'Male'),
+(7, 'Tetsuya Kuroko', 'Male'),
+(8, 'Taiga Kagami', 'Male'),
+(9, 'Teppei Kiyoshi', 'Male'),
+(10, 'Junpei Hyūga', 'Male'),
+(11, 'Shun Izuki', 'Male'),
+(12, 'Rinnosuke Mitobe', 'Male'),
+(13, 'Shinji Koganeie', 'Male'),
+(14, 'Satoshi Tsuchida', 'Male'),
+(15, 'Kōki Furihata', 'Male'),
+(16, 'Kōichi Kawahara', 'Male'),
+(17, 'Hiroshi Fukuda', 'Male'),
+(18, 'Yukio Kasamatsu', 'Male'),
+(19, 'Yoshitaka Moriyama', 'Male'),
+(20, 'Mitsuhiro Hayakawa', 'Male'),
+(21, 'Kōji Kobori', 'Male'),
+(22, 'Shinya Nakamura', 'Male'),
+(23, 'Kazunari Takao', 'Male'),
+(24, 'Taisuke Ōtsubo', 'Male'),
+(25, 'Kiyoshi Miyaji', 'Male'),
+(26, 'Shinsuke Kimura', 'Male'),
+(27, 'Yūya Miyaji', 'Male'),
+(28, 'Shoichi Imayoshi', 'Male'),
+(29, 'Ryō Sakurai', 'Male'),
+(30, 'Kōsuke Wakamatsu', 'Male'),
+(31, 'Yoshinori Susa', 'Male'),
+(32, 'Tatsuya Himuro', 'Male'),
+(33, 'Wei Liu', 'Male'),
+(34, 'Reo Mibuchi', 'Male'),
+(35, 'Kotarō Hayama', 'Male'),
+(36, 'Eikichi Nebuya', 'Male'),
+(37, 'Chihiro Mayuzumi', 'Male'),
+(38, 'Shōgo Haizaki', 'Male'),
+(39, 'Hideki Ishida', 'Male'),
+(40, 'Kazuhiro Mochizuki', 'Male'),
+(41, 'Makoto Hanamiya', 'Male'),
+(42, 'Kōjiro Furuhashi', 'Male'),
+(43, 'Kazuya Hara', 'Male'),
+(44, 'Kazuya Hara', 'Male'),
+(45, 'Kentarō Seto', 'Male'),
+(46, 'Hiroshi Yamazaki', 'Male'),
+(47, 'Hiroshi Yamazaki', 'Male'),
+(48, 'Tomoki Tsugawa', 'Male'),
+(49, 'Tsutomu Iwamura', 'Male'),
+(50, 'Ryūhei Kasuga', 'Male'),
+(51, 'Yoshikasu Ōmurō', 'Male'),
+(52, 'Kenjirō Sakamoto', 'Male'),
+(53, 'Daisuke Narumi', 'Male'),
+(54, 'Yōhei Kawase', 'Male'),
+(55, 'Masahiro Tsubuku', 'Male'),
+(56, 'Hiroshi Sakuma', 'Male'),
+(57, 'Kazuki Tōyama', 'Male'),
+(58, 'Takuma Ōtsuka', 'Male'),
+(59, 'Yū Tabata', 'Male'),
+(60, 'Masami Meguro', 'Male'),
+(61, 'Kazuma Shinagawa', 'Male'),
+(62, 'Daichi Ueno', 'Male'),
+(63, 'Papa Mbaye Siki', 'Male'),
+(64, 'Yūsuke Tanimura', 'Male'),
+(65, 'Keisuke Kobayashi', 'Male'),
+(66, 'Riko Aida', 'Female'),
+(67, 'Genta Takeuchi', 'Male'),
+(68, 'Masaaki Nakatani', 'Male'),
+(69, 'Katsunori Harasawa', 'Male'),
+(70, 'Masako Araki', 'Female'),
+(71, 'Eiji Shirogane', 'Male'),
+(72, 'Yukinori Matsumoto', 'Male'),
+(73, 'Kōji Hiraiwa', 'Male'),
+(74, 'Kenji Takeda', 'Male'),
+(75, 'Kagetora Aida', 'Male'),
+(76, 'Satsuki Momoi', 'Female'),
+(77, 'Tetsuya #2', 'Other'),
+(78, 'Tanaka', 'Male'),
+(79, 'Alexandra Garcia', 'Female'),
+(80, 'Shūzō Nijimura', 'Male'),
+(81, 'Naoto Sanada', 'Male'),
+(82, 'Kōzō Shirogane', 'Male'),
+(83, 'Shigehiro Ogiwara', 'Male'),
+(84, 'Genius Twins', 'Male'),
+(85, 'Reiji Mochida', 'Male'),
+(86, 'Taichi', 'Male'),
+(87, 'Tomoya Inoue', 'Male'),
+(88, 'Akane Koganei', 'Female'),
+(89, 'Daigo Matsuoka', 'Male'),
+(90, 'Masaya Kubotaa', 'Male'),
+(91, 'Tōru Sekiguchi', 'Male'),
+(92, 'Nash Gold Jr.', 'Male'),
+(93, 'Jason Silver', 'Male');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
